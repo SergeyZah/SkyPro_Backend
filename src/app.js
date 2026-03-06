20d3e790e467a3fe358baded650e9a527b2fc819
Sergey_Zaharychev <zaharychevs@yandex.ru>
2026-03-06 18:11:30 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { PORT = 3000, API_URL = 'http://localhost' } = process.env;
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.status(200);
+    response.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+    console.log('Ссылка на сервер: ${API_URL}:${PORT}');
+});
